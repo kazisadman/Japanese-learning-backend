092deca529b62f30a249f123c226de4036357ca1
teethw <kazisadman01@gmail.com>
2024-12-13 04:54:14 +0600

    deploy

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://japanese-learning-client.vercel.app/",
     credentials: true,
   })
 );
